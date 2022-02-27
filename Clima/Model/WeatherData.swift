c9df90298f5f0a1c5f183d22b09ce7dac4f09a06
brGuirra <br.guirra@gmail.com>
2022-02-27 14:31:27 -0300

    feat: parse the json data

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Bruno Guirra on 27/02/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+
+struct Weather: Decodable {
+    let description: String
+    let id: Int
+}
--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,36 +9,80 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&appid=324a72909dbe3b99ad76db523966f8e1"
+    private var apiKey: String {
+        get {
+            guard let filePath = Bundle.main.path(forResource: "OpenWeather-Info", ofType: "plist") else {
+                fatalError("Couldn't find file 'OpenWeather-Info'.")
+            }
+            
+            let plist = NSDictionary(contentsOfFile: filePath)
+            
+            guard let value = plist?.object(forKey: "API_KEY") as? String else {
+                fatalError("Couldn't find 'API_KEY' in 'OpenWeather-Info'.")
+            }
+            
+            return value
+        }
+    }
+    
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather"
     
     func fetchWeather(cityName: String) {
-        let urlString = "\(weatherURL)&q=\(cityName)"
+        let urlString = "\(weatherURL)?q=\(cityName)&appid=\(apiKey)"
         
         performRequest(urlString: urlString)
     }
     
     func performRequest(urlString: String) {
-        
-        // 1. Create an URL
         if let url = URL(string: urlString) {
-            
-            // 2. Create an URLSession
             let session = URLSession(configuration: .default)
             
-            // 3. Give the session a task
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
                     return
                 }
                 
                 if let data = data {
-                    let dataString = String(data: data, encoding: .utf8 )
-                    print(dataString!)
+                    self.parseJSON(data: data)
                 }
             }
             
-            // 4. Start the task
             task.resume()
         }
+    }
+    
+    func parseJSON(data: Data) {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: data)
+            let id = decodedData.weather[0].id
+            
+            print(getConditionName(conditionId: id))
+        } catch {
+            print(error)
+        }
+    }
+    
+    func getConditionName(conditionId: Int) -> String {
+        switch conditionId {
+            case 200...232:
+                return "cloud.bolt"
+            case 300...321:
+                return "cloud.drizzle"
+            case 500...531:
+                return "cloud.rain"
+            case 600...622:
+                return "cloud.snow"
+            case 701...781:
+                return "cloud.fog"
+            case 800:
+                return "sun.max"
+            case 801...804:
+                return "cloud.bolt"
+            default:
+                return "cloud"
+        }
+
     }
 }
